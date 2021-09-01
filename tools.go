39b8888f66cadef67dcd85d1e62b46127ad55bec
xuwenxiong <xuwenxiong@bytedance.com>
2021-09-01 21:06:40 +0800

    zlib

--- tools.go
+++ tools.go
@@ -566,3 +566,19 @@ func Rc4Decrypt(key,input,output []byte)error{
 	c.XORKeyStream(output, input)
 	return nil
 }
+/*zlib*/
+func ZlibCompress(src []byte) []byte {
+	var in bytes.Buffer
+	w := zlib.NewWriter(&in)
+	w.Write(src)
+	w.Close()
+	return in.Bytes()
+}
+
+func ZlibUnCompress(compressSrc []byte) []byte {
+	b := bytes.NewReader(compressSrc)
+	var out bytes.Buffer
+	r, _ := zlib.NewReader(b)
+	io.Copy(&out, r)
+	return out.Bytes()
+}
