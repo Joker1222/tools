fb0d00cd20b3696010838fd3c5ae4dbdf66e2411
wenxiong.xu <wenxiong.xu@funplus.com>
2022-08-12 17:51:59 +0800

    stack

---
+++ Stack.go
@@ -0,0 +1,45 @@
+package tools
+
+type (
+	Stack struct {
+		top *snode
+		length int
+	}
+	snode struct {
+		value interface{}
+		prev *snode
+	}
+)
+// Create a new stack
+func New() *Stack {
+	return &Stack{nil,0}
+}
+// Return the number of items in the stack
+func (this *Stack) Len() int {
+	return this.length
+}
+// View the top item on the stack
+func (this *Stack) Peek() interface{} {
+	if this.length == 0 {
+		return nil
+	}
+	return this.top.value
+}
+// Pop the top item of the stack and return it
+func (this *Stack) Pop() interface{} {
+	if this.length == 0 {
+		return nil
+	}
+
+	n := this.top
+	this.top = n.prev
+	this.length--
+	return n.value
+}
+
+// Push a value onto the top of the stack
+func (this *Stack) Push(value interface{}) {
+	n := &snode{value,this.top}
+	this.top = n
+	this.length++
+}
